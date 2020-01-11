9490f5df2a1bb76b6268be6698720916f4e28b9e
Vidya <prabhuvidyap01@gmail.com>
2020-01-11 17:41:12 +0530

    jpql

---
+++ JDBC_APP/crud/UpdateEmployee.java
@@ -0,0 +1,31 @@
+package com.bcits.jpawithhibernateapp.crud;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.bcits.jpawithhibernateapp1.bean.EmployeePrimaryInfo;
+
+public class UpdateEmployee {
+	public static void main(String[] args) {
+		EntityManager manager = null;
+		EntityTransaction transaction = null;
+		try {
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test");
+			manager = entityManagerFactory.createEntityManager();
+			transaction = manager.getTransaction();
+			transaction.begin();
+			EmployeePrimaryInfo info = manager.find(EmployeePrimaryInfo.class, 104);
+			info.setSalary(35000);
+			transaction.commit();
+			System.out.println("Record Updated");
+		} catch (Exception e) {
+			e.printStackTrace();
+			transaction.rollback();
+		} finally {
+			manager.close();
+		}
+	}
+
+}
