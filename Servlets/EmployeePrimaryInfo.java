11adaba16409fd707d508ab0a14549248f788f97
Vidya <prabhuvidyap01@gmail.com>
2020-01-15 17:10:43 +0530

    servlet

---
+++ Servlets/EmployeePrimaryInfo.java
@@ -0,0 +1,114 @@
+package com.bcits.empwebapp.bean;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.*;
+
+@Data
+@Entity
+@Table(name = "employee_primary_info")
+public class EmployeePrimaryInfo implements Serializable {
+	@Id
+	@Column(name="emp_id")
+	private int empid;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(long mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public Date getDateOfJoining() {
+		return dateOfJoining;
+	}
+	public void setDateOfJoining(Date dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public int getDeptID() {
+		return deptID;
+	}
+	public void setDeptID(int deptID) {
+		this.deptID = deptID;
+	}
+	public int getManagerID() {
+		return managerID;
+	}
+	public void setManagerID(int managerID) {
+		this.managerID = managerID;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Column
+	private String name;
+	@Column(name="mobile")
+	private long mobileNo;
+	@Column
+	private String email;
+	@Column(name="date_of_birth")
+	private Date dateOfBirth;
+	@Column(name= "date_of_joining")
+	private Date dateOfJoining;
+	@Column
+	private String designation;
+	@Column(name="blood_group")
+	private String bloodGroup;
+	@Column
+	private double salary;
+	@Column(name="dept_id")
+	private int deptID;
+	@Column(name="manager_id")
+	private int managerID;
+	@Column
+	private String password;
+}
